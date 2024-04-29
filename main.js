9cd493aec97d2c61071dd415f7c7454366f36d76
antonioalves1996 <168119183+antonioalves1996@users.noreply.github.com>
2024-04-29 09:24:24 -0300

    Arquivos necessários para o site

---
+++ main.js
@@ -0,0 +1,10 @@
+var images = ["https://blog.megaoffice.net/wp-content/uploads/2019/10/293880-entenda-as-diferencas-da-contabilidade-fiscal-e-gerencial.jpg", "https://site.erpsoft.com.br/wp-content/uploads/2020/08/2847446-1-scaled.jpg"]; // URLs das imagens
+var currentIndex = 0;
+var interval = 10000; // 10 segundos
+
+function changeImage() {
+  currentIndex = (currentIndex + 1) % images.length;
+  document.getElementById("background").style.backgroundImage = "url('" + images[currentIndex] + "')";
+}
+
+setInterval(changeImage, interval);
