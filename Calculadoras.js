4d2f5e5c4c23cfe3a92862529d1d248a33ae4013
antonioalves1996 <168119183+antonioalves1996@users.noreply.github.com>
2024-05-01 16:51:10 -0300

    Arquivos para novo layout

---
+++ Calculadoras.js
@@ -0,0 +1,15 @@
+document.getElementById('btnPF').addEventListener('click', function() {
+    window.location.href = 'Caculadora_PF.html';
+});
+
+document.getElementById('btnPJ').addEventListener('click', function() {
+    window.location.href = 'Calculadora_PJ.html';
+});
+
+document.getElementById('btnPJ').addEventListener('click', function() {
+    window.location.href = 'Calculadora_PJ.html';
+});
+
+document.getElementById('btnPJ').addEventListener('click', function() {
+  window.location.href = 'Calculadora_PJ.html';
+});
