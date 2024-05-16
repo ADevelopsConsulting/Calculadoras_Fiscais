bad33323be27c41db6b7310f88817d8ce5ccb58b
antonioalves1996 <168119183+antonioalves1996@users.noreply.github.com>
2024-05-16 08:33:02 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,14 +1,16 @@
-var images = ["https://blog.megaoffice.net/wp-content/uploads/2019/10/293880-entenda-as-diferencas-da-contabilidade-fiscal-e-gerencial.jpg", "https://site.erpsoft.com.br/wp-content/uploads/2020/08/2847446-1-scaled.jpg"]; // URLs das imagens
-var currentIndex = 0;
-var interval = 10000; // 10 segundos
+document.addEventListener("DOMContentLoaded", function() {
+  var images = ["https://blog.megaoffice.net/wp-content/uploads/2019/10/293880-entenda-as-diferencas-da-contabilidade-fiscal-e-gerencial.jpg", "https://site.erpsoft.com.br/wp-content/uploads/2020/08/2847446-1-scaled.jpg"];
+  var currentIndex = 0;
+  var interval = 10000; // 10 segundos
 
-function changeImage() {
-  currentIndex = (currentIndex + 1) % images.length;
-  document.getElementById("background").style.backgroundImage = "url('" + images[currentIndex] + "')";
-}
+  function changeImage() {
+    currentIndex = (currentIndex + 1) % images.length;
+    document.getElementById("background").style.backgroundImage = "url('" + images[currentIndex] + "')";
+  }
 
-setInterval(changeImage, interval);
+  setInterval(changeImage, interval);
 
-document.getElementById("calculadorasButton").addEventListener("click", function() {
-  window.location.href = "Calculadoras.html";
+  document.getElementBy("acessarCalculadorasButton").addEventListener("click", function() {
+    window.location.href = "Calculadoras.html";
+  });
 });
